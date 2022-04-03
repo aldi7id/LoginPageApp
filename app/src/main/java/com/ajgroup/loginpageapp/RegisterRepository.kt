cc151ba7481a0df96a0aaac1514a8d7f4a0ebbe7
aldi7id <aldi7.indo@gmail.com>
2022-04-03 16:47:17 +0700

    add RegisterRepository.kt

---
+++ app/src/main/java/com/ajgroup/loginpageapp/RegisterRepository.kt
@@ -0,0 +1,14 @@
+package com.ajgroup.loginpageapp
+import android.util.Log
+import androidx.lifecycle.LiveData
+
+class RegisterRepository(private val dao: RegisterDatabaseDao) {
+    val users = dao.getAllUsers()
+
+    suspend fun insert(user: RegisterEntity){
+        return dao.insert(user)
+    }
+    suspend fun getUserName(userName: String):RegisterEntity?{
+        return dao.getUsername(userName)
+    }
+}
